330f228f880995d2012c2a100c4137b01090b87f
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-09-17 14:10:46 -0600

    started pokemon project

---
+++ pokemon/sketch.js
@@ -0,0 +1,21 @@
+// Line Art Demo
+// Nabeel Rahman
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function preload() {
+  guy = loadImage("assets/guy.piskel");
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(250);
+}
+
+function draw() {
+  imageMode(CENTER)
+}
