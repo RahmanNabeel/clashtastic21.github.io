93ee77bedf3cefa94fbe5b6a8c7802b43614ab8c
clashtastic21 <rahmannabeel64@gmail.com>
2019-10-16 22:03:40 -0600

    update

--- tictactoe/sketch.js
+++ tictactoe/sketch.js
@@ -10,50 +10,104 @@ let y;
 let dx;
 let dy;
 let radius = 100;
-let rectSize = 100; 
-let state = 'menu';
+let rectSize = 100;
+let state = "menu";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  
   x = width/2;
   y = height/2;
+  dx = random(-15, 15);
+  dy = random(-15, 15);
 }
 
 function draw() {
-  background(220)
+  background(255);
 
-  if (state === 'menu') {
+  if (state === "menu") {
     showMenu();
+    checkIfButtonClicked();
   }
-
+  else if (state === "circle") {
+    moveShape();
+    displayCircle();
+  }
+  else if (state === "rectangle") {
+    moveShape();
+    displayRectangle();
+  }
 }
 
+
 function showMenu() {
+  // show rectangle button
   rectMode(CENTER);
-  fill(255, 0, 125);
-  rect(width/2, height/2 - 100, 400, 150)
+  fill(255, 0, 0, 125);
+  rect(width/2, height/2 - 100, 400, 150);
   textAlign(CENTER, CENTER);
   textSize(50);
-  text("Game 1", width/2, height/2 - 100);
-
-  fill(255, 0, 125);
-  rect(width/2, height/2 - 100, 400, 150)
   fill(0);
-  text("Game 1", width/2, height/2 - 100);
+  text("Rectangle", width/2, height/2 - 100);
+
+  // show circle button
+  fill(255, 0, 0, 125);
+  rect(width/2, height/2 + 100, 400, 150);
+  fill(0);
+  text("Circle", width/2, height/2 + 100);
 }
 
 function checkIfButtonClicked() {
   if (mouseIsPressed) {
-    //check rectangle button
-    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 - 100 - 75 && mouseY < height/2 - 100 + 75) {
-      state = 'Game 1';
+    // check for rectangle button
+    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 &&
+        mouseY > height/2 - 100 - 75 && mouseY < height/2 - 100 + 75) {
+          state = "rectangle";
+    }
+
+    // check for circle button
+    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 &&
+      mouseY > height/2 + 100 - 75 && mouseY < height/2 + 100 + 75) {
+        state = "circle";
     }
   }
-  if (mouseIsPressed) {
-    //check circle button
-    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 + 100 - 75 && mouseY < height/2 + 100 + 75) {
-      state = 'Game 1';
-    }
+}
+
+
+function windowResized() {
+  setup();
+}
+
+function moveShape() {
+  // move
+  x += dx;
+  y += dy;
+}
+
+function displayCircle() {
+  // bounce if needed
+  if (x > width - radius/2 || x < 0 + radius/2) {
+    dx *= -1;
   }
+
+  if (y > height - radius/2 || y < 0 + radius/2) {
+    dy *= -1;
+  }
+
+  fill(0);
+  circle(x, y, radius);
+}
+
+function displayRectangle() {
+  // bounce if needed
+  if (x > width - rectSize || x < 0) {
+    dx *= -1;
+  }
+
+  if (y > height - rectSize || y < 0) {
+    dy *= -1;
+  }
+
+  fill(0);
+  rect(x, y, rectSize, rectSize);
 }
