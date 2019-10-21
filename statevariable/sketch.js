0d54785283222a431a4d3a5a96a2577613f72d5a
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-10-21 14:11:29 -0600

    update

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -1,6 +1,6 @@
 // State Variables
 // Nabeel Rahman
-// Date
+// 10/21/2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,6 +8,8 @@
 let b = 0;
 let c = 0;
 let state = 'normal';
+let color = [];
+let outsideColor = [];
 
 function setup() { 
   createCanvas(600, 600);
@@ -24,6 +26,13 @@ function draw() {
   }
 }
 
+
+
+
+
+
+
+
 function normal() {
   fill(0, 0);
   stroke(0, 190);
@@ -34,9 +43,32 @@ function normal() {
       
       let d = dist(mouseX, mouseY, i, j);
       ellipse(i, j, d * 0.2);
- 
     }
   }
+}
+
+function invert() {
+  fill(255, 255);
+  stroke(0, 190);
+  strokeWeight(2);
+  let spacing = 20;
+  for (let i = spacing/2; i < width; i += spacing) {
+    for (let j = spacing/2; j < height; j += spacing) {
+      
+      let d = dist(mouseX, mouseY, i, j);
+      ellipse(i, j, d * 0.2);
+    }
+  }
+}
+
+function mousePressed() {
+  state = 'invert';
+}
+
+function keyTyped() {
+if (key === 'a') {
+  state = 'normal'; 
+}
 }
 
 function invert() {
