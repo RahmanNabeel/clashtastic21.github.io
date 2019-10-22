d55fc57ef13c0463c440e30a4d06548dc2157651
clashtastic21 <rahmannabeel64@gmail.com>
2019-10-21 22:18:22 -0600

    final

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -3,18 +3,21 @@
 // 10/21/2019
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - Added some html to the website
 
+// Global Variables 
 let b = 0;
 let c = 0;
 let state = 'normal';
 let color = [];
-let outsideColor = [];
 
+//Setup 
 function setup() { 
-  createCanvas(600, 600);
+  createCanvas(700, 600);
+
 } 
 
+//Draw code with state variable
 function draw() { 
   background(50);
   
@@ -26,15 +29,9 @@ function draw() {
   }
 }
 
-
-
-
-
-
-
-
+//If state is normal
 function normal() {
-  fill(0, 0);
+  fill(b, c);
   stroke(0, 190);
   strokeWeight(2);
   let spacing = 20;
@@ -47,6 +44,7 @@ function normal() {
   }
 }
 
+//If state is invert
 function invert() {
   fill(255, 255);
   stroke(0, 190);
@@ -61,34 +59,12 @@ function invert() {
   }
 }
 
+//Changes color 
 function mousePressed() {
   state = 'invert';
 }
 
-function keyTyped() {
-if (key === 'a') {
-  state = 'normal'; 
-}
-}
-
-function invert() {
-  fill(255, 255);
-  stroke(0, 190);
-  strokeWeight(2);
-  let spacing = 20;
-  for (let i = spacing/2; i < width; i += spacing) {
-    for (let j = spacing/2; j < height; j += spacing) {
-      
-      let d = dist(mouseX, mouseY, i, j);
-      ellipse(i, j, d * 0.2);
-    }
-  }
-}
-
-function mousePressed() {
-  state = 'invert';
-}
-
+//Changes color back to original
 function keyTyped() {
 if (key === 'a') {
   state = 'normal'; 
