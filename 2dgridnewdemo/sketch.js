758d052e8a41c7794386f823f03f3d016d9dff57
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-10-24 14:15:31 -0600

    2d array grid redux

---
+++ 2dgridnewdemo/sketch.js
@@ -0,0 +1,78 @@
+let grid;
+let rows = 30;
+let cols = 30;
+
+function setup() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  grid = createRandom2dArray(cols, rows);
+}
+
+function draw() {
+  background(220);
+  displayGrid(grid, rows, cols);
+}
+
+function windowResized()
+
+function keyTyped() {
+  if (key === 'r') {
+    grid = createRandom2dArray(cols, rows);
+  }
+  if (key === 'c') {
+    for (let x = 0; x < cols; x++) {
+      for (let y = 0; y < rows; y++) {
+        grid[y][x] = 1;
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  let cellSize = width/cols;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
+  }
+  else {
+    grid[yCoord][xCoord] = 1;
+  }
+}
+
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(1);
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
+}
