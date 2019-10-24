e2f3c74c9aeca6987065e6ed75c146d1915f061c
clashtastic21 <rahmannabeel64@gmail.com>
2019-10-23 22:58:19 -0600

    2d grid

--- 2dgrid/sketch.js
+++ 2dgrid/sketch.js
@@ -1,5 +1,6 @@
 let grid = [];
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = create2dArray(20, 20);
@@ -11,9 +12,9 @@ function draw() {
 }
 
 function displayGrid(theGrid) {
-  //assumes the grid is a square
+  //assumes the grid is a square...
   for (let y = 0; y < theGrid[0].length; y++) {
-    for (let x = 0; x < theGrid[0].length; x++){
+    for (let x = 0; x < theGrid[0].length; x++) {
       if (theGrid[y][x] === 0) {
         fill(255);
       }
@@ -28,10 +29,10 @@ function displayGrid(theGrid) {
 
 function create2dArray(cols, rows) {
   let someArray = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i=0; i<cols; i++) {
     someArray.push([]);
-    for (let j = 0; j < rows; j++) {
-      if (random(100) < 50){
+    for (let j=0; j<rows; j++) {
+      if (random(100) < 50) {
         someArray[i].push(1);
       }
       else {
