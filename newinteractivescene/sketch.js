592900d8e05b92200723379e0a99073a6dd4a601
clashtastic21 <rahmannabeel64@gmail.com>
2019-10-26 18:16:27 -0600

    update

---
+++ newinteractivescene/sketch.js
@@ -0,0 +1,35 @@
+// New Interactive Scene
+// Nabeel Rahman
+// 10/27/2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+function keyTyped() {
+  if (key === 'r') {
+    background(255);
+  }
+  if (key === 'f') {
+    stroke(0);
+  }
+  if (key === 'a') {
+    stroke(0, 255, 0);
+  }
+  if (key === 's') {
+    stroke(255, 0, 0);
+  }
+  if (key === 'd') {
+    stroke(0, 0, 255);
+  }
+}
+
