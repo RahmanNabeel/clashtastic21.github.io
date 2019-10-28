76605b9483394ee58ad5174fa9c58c7df0110741
clashtastic21 <rahmannabeel64@gmail.com>
2019-10-27 22:25:59 -0600

    update

--- newinteractivescene/sketch.js
+++ newinteractivescene/sketch.js
@@ -1,17 +1,19 @@
-// New Interactive Scene
+// New Interactive Scene - Art
 // Nabeel Rahman
 // 10/27/2019
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - Used Mouse Wheel function to change the stroke weight
 
 let strokeW = 1;
 
+// Set up
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 }
 
+// Draw when mouse is pressed down
 function draw() {
   strokeWeight(strokeW);
   if (mouseIsPressed === true) {
@@ -19,20 +21,37 @@ function draw() {
   }
 }
 
+// Keyboard interaction to change color and reset
 function keyTyped() {
   if (key === 'r') {
     background(255);
+    strokeWeight(strokeW);
   }
   if (key === 'f') {
     stroke(0);
+    strokeWeight(strokeW);
   }
   if (key === 'a') {
     stroke(0, 255, 0);
+    strokeWeight(strokeW);
   }
   if (key === 's') {
     stroke(255, 0, 0);
+    strokeWeight(strokeW);
   }
   if (key === 'd') {
     stroke(0, 0, 255);
+    strokeWeight(strokeW);
   }
+}
+
+// Mouse wheel function to change stroke weight
+function mouseWheel(event) {
+  if (event.delta < 0) {
+    strokeW += 0.5;
+  }
+  else {
+    strokeW -= 0.5;
+  }
+  return false;
 }
