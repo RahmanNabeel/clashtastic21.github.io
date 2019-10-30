4374d9b7366dd888ac1f6e0ebb9e48e363ac2cff
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-10-30 14:11:17 -0600

    update

---
+++ lineart copy/sketch.js
@@ -0,0 +1,16 @@
+// Line Art Demo
+// Nabeel Rahman
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
