6978b25f853efb70361ae7c8156d00d1d5bf9fd4
clashtastic21 <rahmannabeel64@gmail.com>
2019-11-11 19:17:23 -0600

    update

--- wsadmovement/sketch.js
+++ wsadmovement/sketch.js
@@ -1,89 +1,145 @@
-// WASD in Grid Demo
-// Grid assignment
+// Grid Assignment
+// Nabeel Rahman
+// Date
+//
+// Extra for Experts:
 
-let grid;
-let rows = 30;
-let cols = 30;
-let playerX = 15;
-let playerY = 15;
+
+let board =
+  [
+  ['','',''],
+  ['','',''],
+  ['','',''],
+];
+
+let players = ['X', 'O']
+let currentPlayer;
+let available = [];
 
 function setup() {
-  if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
+  createCanvas(400, 400);
+  currentPlayer = 0;
+  for (let j = 0; j < 3; j++){
+    for (let i = 0; i < 3; i++) {
+      available.push([i,j]);
+    }
+  }
+}
+
+function equals3(a, b, c) {
+  return (a==b && b == c && a != '');
+}
+
+function enemyTurn() {
+  let index = floor(random(available.length));
+  let spot = available.splice(index, 1)[0];
+  let i = spot[0];
+  let j = spot[1];
+  board[i][j] = players[currentPlayer];
+  currentPlayer = 0;
+}
+
+function nextTurn() {
+  if (currentPlayer == 0){
+ let my = floor(map(mouseX, 0, width, 0, 3))
+ let mx = floor(map(mouseY, 0, height, 0, 3))
+ let index = [mx, my]
+for (let i = 0; i < available.length; i++){
+  if (index[0] == available[i][0] && index[1] == available[i][1]) {
+      available.splice(i, 1);
+      let j = index[0];
+      let k = index[1];
+      board[j][k] = players[0]
+      currentPlayer = 1;
+  }
+
+}
+}
+}
+
+function checkWinner() {
+  let winner = null
+  let w = width / 3;
+  let h = height / 3;
+
+for (let i = 0; i < 3; i++) {
+  if (equals3(board[i][0], board[i][1], board[i][2])) {
+    let y = h * i + h / 2;
+    line (0, y, width, y);
+    winner = board[i][0];
+    return winner;
+  }
+}
+
+for (let i = 0; i < 3; i++) {
+  if (equals3(board[0][i], board[1][i], board[2][i])) {
+    let x = w * i + w / 2;
+    line (x, 0, x, height);
+    winner = board[0][i];
+    return winner;
+  }
+}
+
+if (equals3(board[0][0], board[1][1], board[2][2])) {
+  winner = board[0][0];
+  line(0,0,width,height);
+  return winner;
+}
+if (equals3(board[2][0], board[1][1], board[0][2])) {
+  winner = board[2][0];
+  line(0, height, width, 0);
+  return winner;
+}
+
+
+  if (winner == null && available.length == 0) {
+    return 'tie';
   }
   else {
-    createCanvas(windowWidth, windowWidth);
+    return winner
   }
-  grid = createEmptyGrid(cols, rows);
-  grid[playerY][playerX] = 1;
+}
+
+function mousePressed() {
+nextTurn();
 }
 
 function draw() {
-  background(220);
-  displayGrid(grid, rows, cols);
-}
+  background(51);
+  let w = width / 3;
+  let h = height / 3;
+  strokeWeight(4);
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+  for (let j = 0; j < 3; j++){
+    for (let i = 0; i < 3; i++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[j][i];
+      textSize(32)
+      if (spot == players[1]) {
+        noFill();
+        ellipse(x, y, w / 2);
+      }
+      else if (spot == players[0]){
+        let xr = w / 4;
+        let yr = h / 4;
+        line(x - xr, y - yr, x + xr, y + yr);
+        line(x + xr, y - yr, x - xr, y + yr);
+      }
 
-class Snake {
-  constructor(x, y, length) {
-    this.x = x;
-    this.y = y;
-    this.length = 1;
-  }
-}
-
-function windowResized() {
-  if (windowWidth > windowHeight) {
-    createCanvas(windowHeight, windowHeight);
-  }
-  else {
-    createCanvas(windowWidth, windowWidth);
-  }
-}
-
-function keyTyped() {
-  // remove player from current spot
-  grid[playerY][playerX] = 0;
-
-  // move the player
-  if (key === "w" && playerY > 0) {
-    playerY -= 1;
-  }
-  if (key === "s" && playerY < rows - 1) {
-    playerY += 1;
-  }
-  if (key === "d" && playerX < cols - 1) {
-    playerX += 1;
-  }
-  if (key === "a" && playerX > 0) {
-    playerX -= 1;
-  }
-
-  // put player back into grid
-  grid[playerY][playerX] = 1;
-}
-
-function createEmptyGrid() {
-  let emptyGrid = [];
-  for (let x = 0; x < cols; x++) {
-    emptyGrid.push([]);
-    for (let y = 0; y < rows; y++) {
-      emptyGrid[x].push(0);
     }
   }
-  return emptyGrid;
-}
-
-function displayGrid(grid, rows, cols) {
-  let cellSize = width / cols;
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
-        fill(255);
-      }
-      else {
-        fill(0);
-      }
-      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+  let result = checkWinner();
+  if (result != null) {
+    createP(result);
+    noLoop();
+  }
+  else {
+    if (currentPlayer == 1){
+      enemyTurn();
     }
   }
 }
