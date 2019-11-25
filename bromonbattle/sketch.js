c663d66765cf0cc0309def05344127be1cb14b5f
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-11-25 14:05:03 -0600

    started work on battle simulator

---
+++ bromonbattle/sketch.js
@@ -0,0 +1,19 @@
+// Major Project - Bromons
+// Abrar Zaher and Nabeel Ramen
+//
+// Main code for battle simulator
+// 
+// 12/30/1867
+
+function setup() {
+  createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
+  background(90);
+}
+
+function draw() {
+
+}
+
+function battleSetup() {
+
+}
