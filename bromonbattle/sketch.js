20269183608103fffd7ab3fff75a9bc9337a8489
Nabeel Rahman <rahmannabeel64@gmail.com>
2019-12-05 14:16:16 -0600

    worked on being able to pick the different options with the arrow keys

--- bromonbattle/sketch.js
+++ bromonbattle/sketch.js
@@ -5,59 +5,76 @@
 // 
 // 12/30/1867
 
+// states for the different options
 let state = "menu"
 
+let wordFill1 = 90;
+let wordFill2 = 0;
+let wordFill3 = 0; 
+let wordFill4 = 0;
+
 function setup() {
+
   createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
   background(192, 192, 192);
-
+  
   console.log(windowHeight);
   console.log(windowWidth);
 }
 
 function draw() {
+
+  // border at the bottom
   displayBattle();
-  displayWords();
+  
+  if (state === "menu") {
+    displayWords();
+  }
 }
 
 function displayBattle() {
+
   // bottom display
   rectMode(CENTER);
   fill(255);
   strokeWeight(7);
   rect(width/2, 3.6*(height/4), width, height/5);
-
-  //line(windowWidth/2.5, windowHeight/10, windowWidth/2.5, windowHeight/10);
+  
+  if (state === "menu") {
+    line(2*(width/3.1), 2*(height/2), 2*(width/3.1), 2*(height/2.5));
+  }
 }
 
 function displayWords() {
+  
   // fight button
-  fill(0);
+  fill(wordFill1);
   textAlign(CENTER, CENTER);
-  textSize(30);
+  textSize(35);
   text("Fight", 3*(width/4), 1.73*(height/2));
 
   // item button
+  fill(wordFill2);
   textAlign(CENTER, CENTER);
-  textSize(30);
+  textSize(35);
   text("Item", 2.98*(width/4), 1.9*(height/2)); 
 
   // bromon button
+  fill(wordFill3);
   textAlign(CENTER, CENTER);
-  textSize(30);
+  textSize(35);
   text("Bromon", 3.6*(width/4), 1.73*(height/2));
 
   // run button
+  fill(wordFill4);
   textAlign(CENTER, CENTER);
-  textSize(30);
+  textSize(35);
   text("Run", 3.499*(width/4), 1.9*(height/2));
 }
 
-function selectButton() {
-  
+function pickOption() {
+
 }
-
-
 
 // function textBox(theText) {
 //   fill(255);
